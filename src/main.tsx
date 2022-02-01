7e5b1cf9c68a47910a475d013448c6b1fbd04d45
Luis García Gómez <garciagomezluis.94@gmail.com>
2022-02-01 00:09:20 -0300

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,13 @@ import './index.css';
 
 import App from './App';
 
+declare global {
+    // eslint-disable-next-line no-unused-vars
+    interface Window {
+        ethereum: any;
+    }
+}
+
 ReactDOM.render(
     <React.StrictMode>
         <App />
